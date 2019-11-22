34cd209a1f323545e3c313780be1a8a56303839d
soojinroh <rsj3112@gmail.com>
2019-11-22 15:25:43 +0900

    feat: init project

---
+++ src/test/java/jpashop/jpabook/JpabookApplicationTests.java
@@ -0,0 +1,16 @@
+package jpashop.jpabook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpabookApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
